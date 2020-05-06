1299111f3544c3049c22be7e7d998d7d1a97b6fa
officeautomation <officeautomationoia@gmail.com>
2020-05-06 15:01:32 +0530

    Added the element at the end of array

--- src/insertAtEnd.java
+++ src/insertAtEnd.java
@@ -1,2 +1,18 @@
-public class insertAtEnd {
+public class insertAtEnd
+{
+    public static void main(String[] args){
+        int n = 10;
+        int element = 50;
+        int Arr[] = {1,2,3,4,5,6,7,8,9,10};
+        int newArr[] = new int[n+1];
+        for(int i = 0; i<n ;i++)
+        {
+           newArr[i] = Arr[i];
+        }
+        newArr[n] = element;
+        for(int i = 0; i<n+1; i++)
+        {
+            System.out.println(newArr[i]);
+        }
+    }
 }
