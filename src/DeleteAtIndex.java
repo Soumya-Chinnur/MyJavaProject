10924c8adc059af0fcb237011ae1ac5a2684ebe4
officeautomation <officeautomationoia@gmail.com>
2020-05-06 14:46:16 +0530

    Deleted element from specified index in an Array

--- src/DeleteAtIndex.java
+++ src/DeleteAtIndex.java
@@ -1,5 +1,33 @@
-public class DeleteAtIndex {
-    public static void main(String args[]){
-
+public class DeleteAtIndex
+{
+    public static void main(String args[])
+    {
+        int[] a = {10,20,30,40,50,60};
+        int del_pos = 2;
+        int count = 0;
+        for(int i =0; i<a.length; i++)
+        {
+            if(del_pos == i)
+            {
+                for(int j = i; j<a.length-1; j++)
+                {
+                    a[j] = a[j+1];
+                }
+                count++;
+                break;
+            }
+        }
+        if(count == 0)
+        {
+            System.out.println("Element not deleted");
+        }
+        else
+        {
+            System.out.println("Element got deleted");
+            for(int i = 0; i< a.length-1; i++)
+            {
+                System.out.println(a[i]);
+            }
+        }
     }
 }
